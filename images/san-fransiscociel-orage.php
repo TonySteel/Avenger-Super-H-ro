9b9f486678eb1d153e85ed7c3f55c0cf413e66f9
Tony Steel <60441474+TonySteel@users.noreply.github.com>
2020-11-14 13:26:17 +0100

    Add files via upload

---
+++ images/san-fransiscociel-orage.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html>
+	<head>
+<link rel="shortcut icon"href="favicon.ico"/>
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js">
+  {lang: 'fr'}
+</script>
+	
+<title>wallpapers free HD,wallpapers free, fonds d'écran gratuits</title>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+<meta http-equiv='Content-Language' content='fr_FR' />
+<meta http-equiv='Content-Script-Type' content='text/javascript' />
+<meta http-equiv='Content-Style-Type' content='text/css' />
+<meta name='keywords' content='wallpapers free to download,fonds d'écran gratuits à télécharger,trouvés sur le Net,la compil par unesourisetmoi.info' />
+<meta name='description' content='wallpapers free to download,the best collection HD and widescreen,paysages et ciel orage en fond ?cran' />
+<meta name='author' content='unesourisetmoi' />
+<meta name='Robots' content='index,follow,all' />
+<meta name='Goglebot' content='index,follow,all' />
+<meta name='googlebot-image' content='index,follow,all' />
+<meta http-equiv='imagetoolbar' content='no' /><!-- disable IE's image toolbar -->
+<link rel="alternate" type="application/rss+xml" title="wallpapers free, fonds d'écran gratuits RSS Feed" href="LightNEasy/rss.php" />
+<link rel='stylesheet' type='text/css' href='templates/net/style.css' />
+<link rel='stylesheet' type='text/css' href='css/lightneasy.css' />
+<link rel="stylesheet" href="css/lytebox.css" type="text/css" media="screen" />
+<script type="text/javascript" src="js/lytebox.js"></script>
+<!-- +++++++++++++++++++++++++++++++++++++++++++++++++
+| LightNEasy Content Management System
+| Copyright 2007-2012 Fernando Baptista
+| http://www.lightneasy.org
+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+| LightNEasy Mini version 2.5.1
+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+| Released under the terms & conditions of v2 of the
+| GNU General Public License. For details refer to
+| the included gpl.txt file or visit http://gnu.org
++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-908906-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+
+<body>
+<div id="menu2">
+		<ul class="menu_left">
+				<li class="home"><a href="http://www.unesourisetmoi.info" onclick="window.open(this.href);return false;" title="fonds d'écran">Fonds d'écran</a></li>
+			<li class="wiki"><a href="http://www.unesourisetmoi.info/bgmenu/depart.php" onclick="window.open(this.href);return false;" title="fonds d'écran gratuits">Fonds d'écran gratuits</a></li>
+			<li class="ressource"><a href="http://www.unesourisetmoi.info/bgmenu/creations.php" onclick="window.open(this.href);return false;" title="créations numériques">Créations numériques</a></li>
+			<li class="forum"><a href="http://www.unesourisetmoi.info/bgmenu/webmaster.php" onclick="window.open(this.href);return false;" title="webmastering">Webmastering</a></li>
+			<li class="blog"><a href="http://longuetraine.fr/index.php?categorie4/referencement-efficace" onclick="window.open(this.href);return false;" title="référencement efficace">Référencement efficace</a></li>
+			<li class="home"><a href="http://refok.fr/index.php?categorie1/booster" onclick="window.open(this.href);return false;" title="outils SEO">outils SEO</a></li>
+		</ul>
+	
+	</div><!-- menu --><br /><br /><p>&nbsp;</p>
+<div id="container">
+
+	<div id="sitename">
+				<h1><a href="./">wallpapers free, fonds d'écran gratuits</a><!--,wallpapers free HD--></h1>
+		<h2>wallpapers free HD,wallpapers free to download, fonds d'écran gratuits  à télécharger,trouvés sur le Net, la compil</h2>
+	</div>
+	<div id="mainmenu">
+				<ul>
+<li class="first"><a class="selected" href="index.php">free wallpapers</a></li>
+<li><a href="paysages-grand-format.php">Paysages</a></li>
+<li><a href="animaux-fonds-ecran-gratuits.php">Animaux</a></li>
+<li><a href="fonds-ecran-gratuits-creation.php">Créatifs</a></li>
+<li><a href="nature-wallpaper-free-fonds-ecran-gratuits.php">Nature</a></li>
+<li><a href="fonds-ecran-3d-gratuits.php">Fonds 3d</a></li>
+<li><a href="vector-wallpaper-free.php">Vector</a></li>
+<li><a href="fonds-ecran-gratuits-hd.php">hd</a></li>
+<li><a href="hd-wallpapers.php">HD Wallpapers</a></li>
+<li><a href="fantasy-wallpapers-free.php">Fantasy Wallpapers</a></li>
+</ul>
+  </div>
+	<div id="mainmenu2">
+				<ul>
+<li><a href="wallpapers-free.php">wallpapers free</a></li><li><a class="selected" href="wallpapers-free-hd.php">wallpapers free HD</a></li><li><a href="wallpapers-free-widescreen.php">wallpapers widescreen</a></li><li><a class="selected" href="wallpapers-free-ciel-orage.php">wallpapers free HD</a></li><li><a href="wallpapers-free-landscape-ciel-orage.php">wallpapers free landscape</a></li><li><a href="wallpapers-free-space-world.php">wallpapers free space and world</a></li><li><a href="contact.php">Contact</a></li><li><a href="news.php">News</a></li></ul>
+  </div>
+ 
+	<div id="wrap">
+	  <div id="leftside">
+				<h2>News</h2>
+		<span class="LNEnews_title" >fonds d'écran HD</span>
+<span class="LNEnews_date">Posted at 04/01/14 - 08:02 PM</span>
+<div class="LNEnews_text"><p>Encore une nouvelles série de fonds d'écran, ou walppapers free, cette fois en HD ou haute définition ! 22 nouveaux fonds d'écran, trouvés sur le net ont été ajoutés aux centaines déjà présentes dans ce dossier; vous allez donc pouvoir changer encore plus facilement le fond d'écran du bureau de votre ordinateur ...</p>
+<p>Ajout récents : <a href="http://www.unesourisetmoi.info/net/fonds-hd">fonds d'écran HD</a>  / <a href="http://www.unesourisetmoi.info/net/spiderman-wallpapers-hd">windows8 spiderman wallpapers</a> / <a href="http://www.unesourisetmoi.info/net/windows-7-wallpapers-free.php">windows 7 wallpapers</a></p></div>
+<span class="LNEnews_author">Author: wallpapers free</span>
+<br />
+		<hr />
+<div id="google_translate_element"></div>
+<script>
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'fr'
+  }, 'google_translate_element');
+}
+</script><script src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+		<hr />
+        <div align="center"><a href="http://www.redacteur-du-web.fr/" title="Rédaction web" target="_blank">Rédacteur web</a>
+          
+        </div>
+        <hr />
+        
+		Votre lien sur toutes les pages du site :
+        <div id="LNE_show">
+<h3>General</h3>
+<ul>
+<li><a href="http://www.unesourisetmoi.info/liensdur/" onclick="window.open(this.href,'_blank');return false;">échange de liens en dur</a><div>script d'échange de liens pour améliorer votre référencement</div></li>
+<li><a href="http://blog.unesourisetmoi.info/led/" onclick="window.open(this.href,'_blank');return false;">échange de liens</a><div>échange de liens en dur sur le blog du référencement efficace</div></li>
+<li><a href="http://www.unesourisetmoi.info/bgmenu/creations.php" onclick="window.open(this.href,'_blank');return false;">créations numériques</a><div>créations numériques,fonds d'écran gratuits,création et référencement de sites web pour tous.</div></li>
+<li><a href="http://blog.unesourisetmoi.info/index.php?categorie10/apprendre-a-se-servir-de-google" onclick="window.open(this.href,'_blank');return false;">apprendre Google</a><div>tout pour apprendre à se servir et bien utiliser Google</div></li>
+<li><a href="http://blog.unesourisetmoi.info/index.php?categorie12/communiques-de-presse" onclick="window.open(this.href,'_blank');return false;">communiqués</a><div>Des communiqués de presse, gratuits et sur simple demande pour valoriser votre site sur le Net.</div></li>
+</ul>
+</div>
+    <div align="center"> <hr /><br />
+
+
+</div>
+		</div>
+	  <div id="rightside">
+			  <div id="navlist">
+			<h1>wallpapers free HD</h1><h2>
+<li class="LNE_menu"><a href="index.php">free wallpapers</a></li>
+<li class="LNE_menu_intend"><a href="wallpapers-free.php">wallpapers free</a></li>
+<li class="LNE_menu_intend"><a class="selected" href="wallpapers-free-hd.php">wallpapers free HD</a></li>
+<li class="LNE_menu_intend"><a href="wallpapers-free-widescreen.php">wallpapers widescreen</a></li>
+<li class="LNE_menu_intend"><a class="selected" href="wallpapers-free-ciel-orage.php">wallpapers free HD</a></li>
+<li class="LNE_menu_intend"><a href="wallpapers-free-landscape-ciel-orage.php">wallpapers free landscape</a></li>
+<li class="LNE_menu_intend"><a href="wallpapers-free-space-world.php">wallpapers free space and world</a></li>
+<li class="LNE_menu_intend"><a href="contact.php">Contact</a></li>
+<li class="LNE_menu_intend"><a href="news.php">News</a></li>
+<li class="LNE_menu"><a href="paysages-grand-format.php">Paysages</a></li>
+<li class="LNE_menu"><a href="animaux-fonds-ecran-gratuits.php">Animaux</a></li>
+<li class="LNE_menu"><a href="fonds-ecran-gratuits-creation.php">Créatifs</a></li>
+<li class="LNE_menu"><a href="nature-wallpaper-free-fonds-ecran-gratuits.php">Nature</a></li>
+<li class="LNE_menu"><a href="fonds-ecran-3d-gratuits.php">Fonds 3d</a></li>
+<li class="LNE_menu"><a href="vector-wallpaper-free.php">Vector</a></li>
+<li class="LNE_menu"><a href="fonds-ecran-gratuits-hd.php">hd</a></li>
+<li class="LNE_menu"><a href="hd-wallpapers.php">HD Wallpapers</a></li>
+<li class="LNE_menu"><a href="fantasy-wallpapers-free.php">Fantasy Wallpapers</a></li>
+</h2></div>
+             <p>Les fonds d'écran ou wallpapers, ou encore papier-peints, permettent 
+très facilement de personnaliser votre espace de travail. Dans ce 
+dossier ou sur le reste du site, vous en trouverez plusieurs milliers en
+ téléchargement gratuit.</p>  <span style="text-decoration:underline"><strong>Il vous suffit de faire un 'clic' droit sur une image et de choisir l'option " ouvrir le lien dans un nouvel onglet ' pour accéder au fond d'écran en taille réelle que vous pourrez alors enregisstrer directement sur votre bureau</strong></span> .... :)	
+              <hr /><br />
+		<div align="center">
+		<img src="http://www.unesourisetmoi.info/pubs/pubwall_250-250_01.jpg" width="150" height="150" alt="pub" /></div>
+                  <hr /><br />
+       <p>Liens &agrave; visiter:<br /> |
+       <a href="http://referencer.unesourisetmoi.info" target="_blank">aide au r&eacute;f&eacute;rencement</a> <br /> 
+       |
+       <a href="http://www.unesourisetmoi.info/screensavers/">screensavers free</a> <br /> |
+       <a href="http://www.unesourisetmoi.info/portofolio/" target="_blank">portfolio</a> <br /> |
+
+       <a href="http://www.unesourisetmoi.info/tunisie/" target="_blank">voyage en Tunisie</a> <br /> |
+       <a href="http://www.unesourisetmoi.info/autos/" target="_blank">Fonds d'écran automobile</a> <br /> |
+       <a href="http://www.unesourisetmoi.info/sexy/" target="_blank">Sexy Wallpapers</a> <br /> |
+       <a href="http://www.unesourisetmoi.info/ze/" target="_blank">Photographies et artistes</a> <br /> |
+       <a href="http://www.unesourisetmoi.info/gifs/" target="_blank">Gifs animés</a> <br /> |
+       <a href="http://www.unesourisetmoi.info/album/" target="_blank">Les indiens d'am&eacute;rique</a> <br /> |
+       <a href="http://www.unesourisetmoi.info/dali/" target="_blank">Salvador Dali</a> <br /> |
+      <a href="http://www.unesourisetmoi.info/agadir/" target="_blank">agadir maroc</a> <br /> 
+      |
+      <a href="http://www.unesourisetmoi.info/mer/" target="_blank">fonds d'&eacute;cran,photos,mer et c&ocirc;te d'azur</a> <br />|
+<a target="_blank" href="http://www.jeux-fille.biz/jeux-cuisine/1.html" title="Jeux de cuisine">Jeux de cuisine gratuit</a> <br />
+|
+
+
+</p>  </div>
+
+	  <div id="content">
+	  <div align="center">
+	  <p align="center">
+<!--Mégabannière 728x90-->
+<!--<script language='javascript' type='text/javascript' src='http://a01.gestionpub.com/GP28147272b2dc97042'></script>-->
+	  </p>
+  
+</div>
+       <p>&nbsp; </p>
+
+<h1 class="LNE_title">wallpapers free HD and widescreen : paysages et ciel d'orage.</h1>
+<p><a href="galeries/4/ciel_orage_sombre_01.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_01.jpg" ><img src="thumbs/ciel_orage_sombre_01.jpg" width="150" alt="ciel_orage_sombre_01.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_02.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_02.jpg" ><img src="thumbs/ciel_orage_sombre_02.jpg" width="150" alt="ciel_orage_sombre_02.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_03.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_03.jpg" ><img src="thumbs/ciel_orage_sombre_03.jpg" width="150" alt="ciel_orage_sombre_03.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_04.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_04.jpg" ><img src="thumbs/ciel_orage_sombre_04.jpg" width="150" alt="ciel_orage_sombre_04.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_05.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_05.jpg" ><img src="thumbs/ciel_orage_sombre_05.jpg" width="150" alt="ciel_orage_sombre_05.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_06.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_06.jpg" ><img src="thumbs/ciel_orage_sombre_06.jpg" width="150" alt="ciel_orage_sombre_06.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_07.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_07.jpg" ><img src="thumbs/ciel_orage_sombre_07.jpg" width="150" alt="ciel_orage_sombre_07.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_08.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_08.jpg" ><img src="thumbs/ciel_orage_sombre_08.jpg" width="150" alt="ciel_orage_sombre_08.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_09.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_09.jpg" ><img src="thumbs/ciel_orage_sombre_09.jpg" width="150" alt="ciel_orage_sombre_09.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_10.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_10.jpg" ><img src="thumbs/ciel_orage_sombre_10.jpg" width="150" alt="ciel_orage_sombre_10.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_11.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_11.jpg" ><img src="thumbs/ciel_orage_sombre_11.jpg" width="150" alt="ciel_orage_sombre_11.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_12.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_12.jpg" ><img src="thumbs/ciel_orage_sombre_12.jpg" width="150" alt="ciel_orage_sombre_12.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_13.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_13.jpg" ><img src="thumbs/ciel_orage_sombre_13.jpg" width="150" alt="ciel_orage_sombre_13.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_14.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_14.jpg" ><img src="thumbs/ciel_orage_sombre_14.jpg" width="150" alt="ciel_orage_sombre_14.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_15.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_15.jpg" ><img src="thumbs/ciel_orage_sombre_15.jpg" width="150" alt="ciel_orage_sombre_15.jpg" class="bordered" /></a>
+<a href="galeries/4/ciel_orage_sombre_16.jpg" rel="lytebox[galeries/4]" title="ciel_orage_sombre_16.jpg" ><img src="thumbs/ciel_orage_sombre_16.jpg" width="150" alt="ciel_orage_sombre_16.jpg" class="bordered" /></a>
+</p>
+
+    
+			
+        <p>&nbsp;</p>
+        <p>&nbsp; </p>
+			
+            <br />
+      votez :  <script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:like href="http://www.unesourisetmoi.info/net/" send="true" layout="button_count" width="80" show_faces="true" font=""></fb:like><!-- Placez cette balise à l'endroit où vous souhaitez que le bouton +1 s'affiche -->
+<g:plusone></g:plusone>  <a href="https://twitter.com/share" class="twitter-share-button" data-via="unesourisetmoi" data-lang="fr">Tweeter</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>  
+<br />
+       suivre sur les médias sociaux : <a href="https://plus.google.com/101180908281844949032/posts?hl=fr" target="_blank"><img src="http://www.unesourisetmoi.info/images/google-plus.png" alt="google+" width="25" height="25" border="0" /></a> <a href="http://www.facebook.com/unesourisetmoi.info" target="_blank"><img src="http://www.unesourisetmoi.info/images/facebook.png" alt="facebook" width="25" height="25" border="0" /></a> <a href="https://twitter.com/unesourisetmoi" target="_blank"><img src="http://www.unesourisetmoi.info/images/twitter.png" alt="twitter" width="25" height="25" border="0" /></a>
+            
+             <br />
+               <p>&nbsp; </p>
+        <p><!--Mégabannière 728x90-->
+<!--<script language='javascript' type='text/javascript' src='http://a01.gestionpub.com/GP28147272b2dc97042'></script></p>-->
+        <p>&nbsp;</p>
+		<h2>wallpapers free HD,wallpapers free to download, fonds d'écran gratuits  à télécharger,trouvés sur le Net, la compil</h2>
+	  </div>
+	<div class="clearingdiv"></div>
+	</div>
+</div>
+<div id="footer">
+  <p>&nbsp;</p>
+  <p>	| <a href="#">haut de page</a> | </p>
+ <p>
+ | <a href="http://www.wallpapers-cinema.com/" target="_blank">Fonds d'écran cinéma</a> | <a href="http://www.wallpapers-avenue.com/" target="_blank">fond d'écran</a> |</p>
+ <p align="center"><a href="http://www.unesourisetmoi.info" target="blank"><img src="http://www.unesourisetmoi.info/net/images/unesourisetmoi.gif" width="249" height="49" alt="fonds d'&eacute;cran" title="fonds d'&eacute;cran" longdesc="http://www.unesourisetmoi.info" /></a></p>   
+       
+|  <a href="?do=sitemap">Plan du site</a> | <img src="../imm/wot.png" width="28" height="28" alt="carte de s&eacute;curit&eacute; wot"><a href="http://www.mywot.com/fr/scorecard/unesourisetmoi.info" title="carte de sécurité wot" target="_blank">Web Of Trust</a> | <a href="http://www.mywot.com/" target="_blank"> <img src = "http://api.mywot.com/widgets/images/badge80x30.png" style = "border: 0" alt = " Free Internet Security - WOT Web of Trust "/></ a> |<p> | <a href="?do=login" rel="nofollow">Login</a> |</p>
+    <!--SlideIn-->
+<!--<script language='javascript' type='text/javascript' src='http://a01.gestionpub.com/GP29a4247fc3293983f'></script>-->
+ </div>
+</div>
+</div>
+</body>
+</html>
